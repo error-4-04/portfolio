f5759f7ee089aef40eb6a413111efe7a5acd9eac
error-4-04 <105510746+error-4-04@users.noreply.github.com>
2022-11-20 10:34:30 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,3 +4,30 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = {
+  pageExtensions: ['NavBar.tsx', 'NavBar.ts', 'NavBar.jsx', 'NavBar.js'],
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'cdn.sanity.io',
+          domains: ['cnd.sanity.io']
+        },
+      ],
+
+    }
+  },
+}
+
